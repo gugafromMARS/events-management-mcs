802de36fc8429f08a118f1ad8e294e14d3f86e51
Gonçalo <goncaloscravo@gmail.com>
2023-09-28 18:01:45 +0100

    Add: delete and update endpoints to user !

--- users-mcs/src/main/java/gsc/projects/usersmcs/controller/UserController.java
+++ users-mcs/src/main/java/gsc/projects/usersmcs/controller/UserController.java
@@ -3,7 +3,8 @@ package gsc.projects.usersmcs.controller;
 
 import gsc.projects.usersmcs.dto.BuyTicketDto;
 import gsc.projects.usersmcs.dto.UserCreateDto;
-import gsc.projects.usersmcs.service.UserServiceImp;
+import gsc.projects.usersmcs.dto.UserUpdateDto;
+import gsc.projects.usersmcs.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,25 +16,36 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
 
-    private UserServiceImp userServiceImp;
+    private final UserService userService;
 
     @GetMapping
     public ResponseEntity<?> getAll(){
-        return ResponseEntity.ok(userServiceImp.getAllUsers());
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<?> getUserById(@PathVariable ("userId") Long userId){
-        return ResponseEntity.ok(userServiceImp.getById(userId));
+        return ResponseEntity.ok(userService.getById(userId));
     }
 
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody UserCreateDto userCreateDto){
-        return new ResponseEntity<>(userServiceImp.create(userCreateDto), HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.create(userCreateDto), HttpStatus.CREATED);
     }
 
     @GetMapping("/{userId}/buyticket")
     public ResponseEntity<?> buyTicket(@PathVariable ("userId") Long userId,@RequestBody BuyTicketDto buyTicketDto){
-        return new ResponseEntity<>(userServiceImp.buyTicketByUserId(userId, buyTicketDto), HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.buyTicketByUserId(userId, buyTicketDto), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> delete(@PathVariable ("userId") Long userId){
+        userService.deleteUserById(userId);
+        return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<?> update(@PathVariable ("userId") Long userId, @RequestBody UserUpdateDto userUpdateDto){
+        return ResponseEntity.ok(userService.updateUser(userId, userUpdateDto));
     }
 }
--- users-mcs/src/main/java/gsc/projects/usersmcs/service/UserService.java
+++ users-mcs/src/main/java/gsc/projects/usersmcs/service/UserService.java
@@ -1,4 +1,23 @@
 package gsc.projects.usersmcs.service;
 
+import gsc.projects.usersmcs.dto.*;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+import java.util.List;
+
 public interface UserService {
+    List<UserDto> getAllUsers();
+
+    UserDto getById(Long userId);
+
+    UserDto create(UserCreateDto userCreateDto);
+
+    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultUserTickets")
+    UserTicketsDto buyTicketByUserId(Long userId, BuyTicketDto buyTicketDto);
+
+    UserTicketsDto getDefaultUserTickets(Long userId, BuyTicketDto buyTicketDto, Exception exception);
+
+    void deleteUserById(Long userId);
+
+    UserDto updateUser(Long userId, UserUpdateDto userUpdateDto);
 }
